6b2773a5722bf09444c8f888a485fc4bf9f0100e
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-08 10:55:32 +0100

    Add files via upload

---
+++ Monitor/Models/MonitorFilterModel.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="MonitorFilterModel.cs" company="DocuSign">
+// Copyright (c) DocuSign. All rights reserved.
+// </copyright>
+
+namespace DocuSign.CodeExamples.Monitor.Models
+{
+    using System;
+
+    public class MonitorFilterModel
+    {
+        public MonitorFilterModel()
+        {
+            this.FieldDataChangedStartDate = DateTimeOffset.Now.AddDays(-10);
+            this.FieldDataChangedEndDate = DateTimeOffset.Now;
+        }
+
+        public DateTimeOffset FieldDataChangedStartDate { get; set; }
+
+        public DateTimeOffset FieldDataChangedEndDate { get; set; }
+    }
+}
