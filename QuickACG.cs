ec7f866c50a2ea8295d40e2ee8b358b9d41a8dc1
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-08 10:52:38 +0100

    Add files via upload

---
+++ QuickACG.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace DocuSign.QuickACG
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var builder = WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+            builder.ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                string path = string.Empty;
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    path = Path.GetFullPath(@"../launcher-csharp/appsettings.json");
+                }
+                else
+                {
+                    path = Path.GetFullPath(@"..\\launcher-csharp\\appsettings.json");
+                }
+                 
+
+                config.AddJsonFile(path,
+                                   optional: false,
+                                   reloadOnChange: true);
+            });
+            return builder;
+        }
+    }
+}
---
+++ eSignature/Controllers/EgController.cs
@@ -0,0 +1,131 @@
+﻿using DocuSign.CodeExamples.Common;
+using DocuSign.CodeExamples.ESignature.Models;
+using DocuSign.CodeExamples.Models;
+using DocuSign.CodeExamples.Views;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Claims;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DocuSign.CodeExamples.Controllers
+{
+    public abstract class EgController : Controller
+    {
+        public abstract string EgName { get; }
+
+        protected CodeExampleText CodeExampleText { get; set; }
+        public DSConfiguration Config { get; }
+        public LauncherTexts LauncherTexts { get; }
+        public IRequestItemsService RequestItemsService { get; }
+
+        public EgController(DSConfiguration config, LauncherTexts launcherTexts, IRequestItemsService requestItemsService)
+        {
+            Config = config;
+            RequestItemsService = requestItemsService;
+            ViewBag.csrfToken = "";
+            LauncherTexts = launcherTexts;
+        }
+
+        [Route("/")]
+        public virtual IActionResult Index()
+        {
+            var identity = this.Request.HttpContext.User.Identity as ClaimsIdentity;
+            if (identity.IsAuthenticated == true)
+            {
+                RequestItemsService.User = RequestItemsService.User ?? new User
+                {
+                    Name = identity.FindFirst(x => x.Type.Equals(ClaimTypes.Name)).Value,
+                    AccessToken = identity.FindFirst(x => x.Type.Equals("access_token")).Value,
+                    RefreshToken = identity.FindFirst(x => x.Type.Equals("refresh_token"))?.Value,
+                    ExpireIn = DateTime.Parse(identity.FindFirst(x => x.Type.Equals("expires_in")).Value)
+                };
+                RequestItemsService.Session = RequestItemsService.Session ?? new Session
+                {
+                    AccountId = identity.FindFirst(x => x.Type.Equals("account_id")).Value,
+                    AccountName = identity.FindFirst(x => x.Type.Equals("account_name")).Value,
+                    BasePath = identity.FindFirst(x => x.Type.Equals("base_uri")).Value
+                };
+            }
+
+            return Redirect("eg001");
+        }
+
+        [Route("/dsReturn")]
+        public IActionResult DsReturn()
+        {
+            return Redirect("eg001");
+        }
+
+        [HttpGet]
+        public virtual IActionResult Get()
+        {
+            // Check that the token is valid and will remain valid for awhile to enable the
+            // user to fill out the form. If the token is not available, now is the time
+            // to have the user authenticate or re-authenticate.
+            bool tokenOk = CheckToken();
+
+            if (tokenOk)
+            {
+                ViewBag.envelopeOk = RequestItemsService.EnvelopeId != null;
+                ViewBag.gatewayOk = Config.GatewayAccountId != null && Config.GatewayAccountId.Length > 25;
+                ViewBag.source = CreateSourcePath();
+                ViewBag.documentation = Config.Documentation + EgName;
+                ViewBag.showDoc = Config.Documentation != null;
+                ViewBag.User = RequestItemsService.User;
+                ViewBag.Session = RequestItemsService.Session;
+                ViewBag.DsConfig = Config;
+                ViewBag.User = RequestItemsService.User;
+                ViewBag.DsConfig = Config;
+                InitializeInternal();
+
+                if (Config.QuickACG == "true" && !(this is Eg001EmbeddedSigningController))
+                {
+                    return Redirect("eg001");
+                }
+
+                return View(EgName, this);
+            }
+
+            return Redirect("/ds/mustAuthenticate");
+        }
+
+        protected virtual void InitializeInternal()
+        {
+            this.ViewBag.CodeExampleText = this.CodeExampleText;
+            this.ViewBag.SupportingTexts = this.LauncherTexts.ManifestStructure.SupportingTexts;
+        }
+
+        public dynamic CreateSourcePath()
+        {
+            var uri = Config.GithubExampleUrl;
+            uri = $"{uri}/eSignature";
+            return $"{uri}/Controllers/{this.GetType().Name}.cs";
+        }
+
+        protected bool CheckToken(int bufferMin = 60)
+        {
+            return RequestItemsService.CheckToken(bufferMin);
+        }
+
+        protected CodeExampleText GetExampleText(string exampleName, ExamplesAPIType examplesAPIType)
+        {
+            int exampleNumber = int.Parse(Regex.Match(exampleName, @"\d+").Value);
+            var groups = this.LauncherTexts.ManifestStructure.APIs
+                .Find(x => x.Name.ToLowerInvariant() == examplesAPIType.ToString().ToLowerInvariant())
+                .Groups;
+
+            foreach (var group in groups)
+            {
+                var example = group.Examples.Find((example) => example.ExampleNumber == exampleNumber);
+
+                if (example != null)
+                {
+                    return example;
+                }
+            }
+
+            return null;
+        }
+    }
+}
