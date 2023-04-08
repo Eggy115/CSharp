70f119aae8d919c798aaf4a4f3b8577e8eb965d9
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-08 10:40:21 +0100

    Add files via upload

---
+++ DSHelper.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="DSHelper.cs" company="DocuSign">
+// Copyright (c) DocuSign. All rights reserved.
+// </copyright>
+
+namespace DocuSign.CodeExamples.Common
+{
+    using System.IO;
+    using System.Runtime.InteropServices;
+
+    internal class DSHelper
+    {
+        internal static string PrepareFullPrivateKeyFilePath(string fileName)
+        {
+            const string DefaultRSAPrivateKeyFileName = "private.key";
+
+            var fileNameOnly = Path.GetFileName(fileName);
+            if (string.IsNullOrEmpty(fileNameOnly))
+            {
+                fileNameOnly = DefaultRSAPrivateKeyFileName;
+            }
+
+            var filePath = Path.GetDirectoryName(fileName);
+            if (string.IsNullOrEmpty(filePath))
+            {
+                filePath = Directory.GetCurrentDirectory();
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) && Directory.GetCurrentDirectory().Contains("bin"))
+            {
+                fileNameOnly = DefaultRSAPrivateKeyFileName;
+                filePath = Path.GetFullPath(filePath);
+            }
+
+            return Path.Combine(filePath, fileNameOnly);
+        }
+
+        internal static byte[] ReadFileContent(string path)
+        {
+            return File.ReadAllBytes(path);
+        }
+    }
+}
---
+++ JWTAuth.cs
@@ -0,0 +1,86 @@
+﻿// <copyright file="JWTAuth.cs" company="DocuSign">
+// Copyright (c) DocuSign. All rights reserved.
+// </copyright>
+
+namespace DocuSign.CodeExamples.Authentication
+{
+    using System;
+    using System.Collections.Generic;
+    using DocuSign.CodeExamples.Common;
+    using DocuSign.eSign.Client;
+    using static DocuSign.eSign.Client.Auth.OAuth;
+
+    public static class JWTAuth
+    {
+        /// <summary>
+        /// Uses Json Web Token (JWT) Authentication Method to obtain the necessary information needed to make API calls.
+        /// </summary>
+        /// <returns>Auth token needed for API calls</returns>
+        public static OAuthToken AuthenticateWithJWT(string api, string clientId, string impersonatedUserId, string authServer, byte[] privateKeyBytes)
+        {
+            var docuSignClient = new DocuSignClient();
+            var apiType = Enum.Parse<ExamplesAPIType>(api);
+            var scopes = new List<string>
+                {
+                    "signature",
+                    "impersonation",
+                };
+            if (apiType == ExamplesAPIType.Rooms)
+            {
+                scopes.AddRange(new List<string>
+                {
+                    "dtr.rooms.read",
+                    "dtr.rooms.write",
+                    "dtr.documents.read",
+                    "dtr.documents.write",
+                    "dtr.profile.read",
+                    "dtr.profile.write",
+                    "dtr.company.read",
+                    "dtr.company.write",
+                    "room_forms",
+                });
+            }
+
+            if (apiType == ExamplesAPIType.Click)
+            {
+                scopes.AddRange(new List<string>
+                {
+                    "click.manage",
+                    "click.send",
+                });
+            }
+
+            if (apiType == ExamplesAPIType.Monitor)
+            {
+                scopes.AddRange(new List<string>
+                {
+                    "signature",
+                    "impersonation",
+                });
+            }
+
+            if (apiType == ExamplesAPIType.Admin)
+            {
+                scopes.AddRange(new List<string>
+                {
+                    "user_read",
+                    "user_write",
+                    "account_read",
+                    "organization_read",
+                    "group_read",
+                    "permission_read",
+                    "identity_provider_read",
+                    "domain_read",
+            });
+            }
+
+            return docuSignClient.RequestJWTUserToken(
+                clientId,
+                impersonatedUserId,
+                authServer,
+                privateKeyBytes,
+                1,
+                scopes);
+        }
+    }
+}
