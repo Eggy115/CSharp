210ff055c2ea3a3d23bca4ca1034d6a21ae5062d
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-08 10:51:32 +0100

    Add files via upload

---
+++ UnitTestTargetProject/MyTargetClass.cs
@@ -0,0 +1,20 @@
+﻿namespace UnitTestTargetProject
+{
+    public class MyTargetClass
+    {
+        public int AddNumbers(int pintFirstNumber, int pintSecondNumber)
+        {
+            return pintFirstNumber + pintSecondNumber;
+        }
+
+        public int SubractNumbers(int pintFirstNumber, int pintSecondNumber)
+        {
+            return pintFirstNumber - pintSecondNumber;
+        }
+
+        public int DivideNumbers(int pintFirstNumber, int pintSecondNumber)
+        {
+            return pintFirstNumber / pintSecondNumber;
+        }
+    }
+}
