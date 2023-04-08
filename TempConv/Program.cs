2079fcef76b761296c2993938ec6db4a5ad2ec42
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-08 10:39:19 +0100

    Add files via upload

---
+++ TempConv/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TempConv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fahrenheit;
+            double celcius;
+            string input;
+
+            Console.WriteLine("Tempperature in F:");
+            input = Console.ReadLine();
+            fahrenheit = double.Parse(input);
+
+            string str = "asdf";
+            foreach (char c in str)
+            {
+                Console.WriteLine(c);
+            }
+
+            celcius = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("The Temperature in C is: " + celcius);
+            Console.ReadLine();
+        }
+    }
+}
